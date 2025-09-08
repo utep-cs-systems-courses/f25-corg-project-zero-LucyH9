af080c2a4563cc05c8b519d15f92c826f272bc39
LucyH9 <velossel@gmail.com>
2025-09-08 17:19:45 -0600

    hello wodrls test :D

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  printf("---------------------------------\n");
+  printf("\nhello wodlr :3\n");
+  printf("---------------------------------\n");
+  
+  return 0;
+
+}
+
+
